2284c75dd5573549f83dcc661df0771c71a54b28
junghyun397 <junghyun397@gmail.com>
2019-06-28 02:21:07 +0900

    Improve Coordinate-Verification Test Structure

--- test/TestAbalone.py
+++ test/TestAbalone.py
@@ -9,21 +9,21 @@ model3 = AbaloneAgent(edge_size=3)
 model5 = AbaloneAgent(edge_size=5)
 
 
+model3_pos_list = [((0, 0), 0), ((1, 1), 4), ((3, 1), 12)]
+
+model5_pos_list = [((0, 0), 0), ((1, 1), 6), ((5, 1), 35), ((6, 2), 43), ((7, 3), 50), ((7, 5), 52), ((8, 4), 56)]
+
+
 class TestAbalone(unittest.TestCase):
 
     def test_2d_2_1d_pos(self):
         # Model 3
-        self.assertEqual(model3.get_1d_pos(0, 0), 0)
-        self.assertEqual(model3.get_1d_pos(1, 1), 4)
-        self.assertEqual(model3.get_1d_pos(3, 1), 12)
+        for n in model3_pos_list:
+            self.assertEqual(model3.get_1d_pos(n[0]), n[1])
 
         # Model 5
-        self.assertEqual(model5.get_1d_pos(1, 1), 6)
-        self.assertEqual(model5.get_1d_pos(5, 1), 35)
-        self.assertEqual(model5.get_1d_pos(6, 2), 43)
-        self.assertEqual(model5.get_1d_pos(7, 3), 50)
-        self.assertEqual(model5.get_1d_pos(7, 5), 52)
-        self.assertEqual(model5.get_1d_pos(8, 4), 56)
+        for n in model5_pos_list:
+            self.assertEqual(model5.get_1d_pos(n[0]), n[1])
 
     def test_valid_pos(self):
         # Model 3
@@ -48,14 +48,12 @@ class TestAbalone(unittest.TestCase):
 
     def test_1d_2_2d_pos(self):
         # Model 3
-        self.assertEqual(model3.get_2d_pos(model3.get_1d_pos(0, 0)), (0, 0))
-        self.assertEqual(model3.get_2d_pos(model3.get_1d_pos(1, 1)), (1, 1))
-        self.assertEqual(model3.get_2d_pos(model3.get_1d_pos(3, 1)), (3, 1))
+        for n in model3_pos_list:
+            self.assertEqual(model3.get_2d_pos(n[1]), n[0])
 
         # Model 5
-        self.assertEqual(model5.get_2d_pos(model5.get_1d_pos(5, 5)), (5, 5))
-        self.assertEqual(model5.get_2d_pos(model5.get_1d_pos(8, 2)), (8, 2))
-        self.assertEqual(model5.get_2d_pos(model5.get_1d_pos(0, 0)), (0, 0))
+        for n in model5_pos_list:
+            self.assertEqual(model5.get_2d_pos(n[1]), n[0])
 
     def test_to_vector(self):
         self.assertTrue(np.equal(model3.game_vector, np.array([0] * (model3.field_size + 5))))
