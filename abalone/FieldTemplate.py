41ad17e88962d8b5a1384c487dad9d92d885b9b7
junghyun397 <junghyun397@gmail.com>
2019-07-09 00:46:12 +0900

    Improve TextDrawer method to use lambda function

--- abalone/FieldTemplate.py
+++ abalone/FieldTemplate.py
@@ -1,33 +1,22 @@
 import numpy as np
 
-from abalone import AbaloneModel
 from abalone.StoneColor import StoneColor
-
-stone_blank = "+"
-stone_black = "@"
-stone_white = "O"
-
-
-def _get_text_shape(code: int) -> str:
-    if code == StoneColor.BLACK.value:
-        return "@"
-    elif code == StoneColor.WHITE.value:
-        return "O"
-    else:
-        return "+"
 
 
 def get_text_board(game_vector: np.ndarray) -> str:
     rs_str = (chr(32) * (game_vector[0] + 3)) + "".join([str(i + 1) + " " for i in range(game_vector[0])])
+    tef = (lambda w: "+" if w == StoneColor.NONE.value else ("@" if w == StoneColor.BLACK.value else "O"))
     for n in range(game_vector[0] * 2 - 1):
         if n < game_vector[0]:
             y = int(((n + game_vector[0] - 1) * (n + game_vector[0]) - (game_vector[0] - 1) * game_vector[0]) / 2)
-            rs_str = "".join(chr(32) * (game_vector[0] - n)) + str(n + 1) + " " \
-                     + "".join([_get_text_shape(game_vector[y + c]) + " " for c in range(game_vector[0] - n)]) + str(game_vector[0] + n + 1) + "\n" + rs_str
+            rs_str = "".join(chr(32) * (game_vector[0] - n)) + chr(65 + n) + " " \
+                     + "".join([tef(game_vector[y + c]) + " " for c in range(game_vector[0] + n)]) \
+                     + (lambda v: "" if v > game_vector[0] - 2 else str(game_vector[0] + v + 1))(n) + "\n" + rs_str
         else:
             y = int((n * (6 * game_vector[0] - n - 5) + -2 * game_vector[0] * (game_vector[0] - 2) - 2) / 2)
-            rs_str = "".join(chr(32) * (n - game_vector[0] + 2)) + " " + str(n + 1) + " " \
-                     + "".join([_get_text_shape(game_vector[y + c]) + " " for c in range(game_vector[0] + n)]) + "\n" + rs_str
+            rs_str = "".join(chr(32) * (n - game_vector[0] + 1)) + " " + chr(65 + n) + " " \
+                     + "".join([tef(game_vector[y + c]) + " " for c in range(game_vector[0] * 3 - n - 2)]) \
+                     + "\n" + rs_str
     return rs_str
 
 
